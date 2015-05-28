00e733ceca01c919168db7343ce5d3b4d56dc0d8
Thibault Hild <thibault.hild@gmail.com>
2015-05-28 15:42:09 +0200

    fix plantuml rendering under osx

--- helpers/plantuml/plantuml.sh
+++ helpers/plantuml/plantuml.sh
@@ -13,6 +13,10 @@ if [ ! -e "${plantjar}" ]; then
 	curl -s -L -o "${plantjar}" "http://downloads.sourceforge.net/project/plantuml/plantuml.jar?r=http%3A%2F%2Fplantuml.sourceforge.net%2Frunning.html&ts=1429274004&use_mirror=switch"
 fi
 
-tmpout="$(mktemp /tmp/plantuml.XXXXXXXX)"
-cat "$@" | java -jar "${plantjar}" -tsvg -pipe > "${tmpout}"
-xdg-open "${tmpout}"
+result="${1%.*}.svg"
+cat "$1" | java -jar "${plantjar}" -tsvg -pipe > "${result}"
+if [ "${OSTYPE::6}" = "darwin" ]; then
+	open -a Safari "${result}"
+else
+	xdg-open "${result}"
+fi
